fb6e067966eca7b6af187f3ddf6970327af58eb0
longle.24hdev <longle.24hdev@gmail.com>
2022-10-23 19:18:41 +0700

    [hotfix]: Fix conflict

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,11 +3,6 @@ import { notiTypes } from "./notification-types.constant";
 import { httpCodes } from "./http-codes.constant";
 import { errors } from "./errors.constant";
 import { messages } from "./messagges.constant";
-<<<<<<< HEAD
 import { infors } from "./infors.constants";
 
 export { notiTypes, httpCodes, roles, errors, messages, infors };
-=======
-
-export { notiTypes, httpCodes, roles, errors, messages };
->>>>>>> ee0b7451de4347f87ae91b1a22834c6e65865503
--- src/services/categoryTopics.service.js
+++ src/services/categoryTopics.service.js
@@ -3,24 +3,13 @@ import { CategoryTopicsRepository } from "repositories";
 class CategoryTopicsService {
   constructor(repo) {
     this.repo = repo;
-<<<<<<< HEAD
-=======
-    this.create = this.create.bind(this);
-    this.update = this.update.bind(this);
-    this.delete = this.delete.bind(this);
-    this.get = this.get.bind(this);
->>>>>>> ee0b7451de4347f87ae91b1a22834c6e65865503
   }
 
   async create(data) {
     try {
       return await this.repo.create(data);
     } catch (error) {
-<<<<<<< HEAD
       throw new Error(error);
-=======
-      throw new Error("CategoryTopic not created");
->>>>>>> ee0b7451de4347f87ae91b1a22834c6e65865503
     }
   }
 
@@ -31,11 +20,7 @@ class CategoryTopicsService {
       }
       return await this.repo.getAll();
     } catch (error) {
-<<<<<<< HEAD
       throw new Error(error);
-=======
-      throw new Error("Can not get category topic");
->>>>>>> ee0b7451de4347f87ae91b1a22834c6e65865503
     }
   }
 
@@ -43,12 +28,7 @@ class CategoryTopicsService {
     try {
       return await this.repo.updateByPk(id, data);
     } catch (error) {
-<<<<<<< HEAD
       throw new Error(error);
-=======
-      console.log(error);
-      throw new Error("CategoryTopic not update");
->>>>>>> ee0b7451de4347f87ae91b1a22834c6e65865503
     }
   }
 
@@ -56,11 +36,7 @@ class CategoryTopicsService {
     try {
       return await this.repo.delete(id);
     } catch (error) {
-<<<<<<< HEAD
       throw new Error(error);
-=======
-      throw new Error("CategoryTopic not delete");
->>>>>>> ee0b7451de4347f87ae91b1a22834c6e65865503
     }
   }
 
@@ -68,11 +44,7 @@ class CategoryTopicsService {
     try {
       return await this.repo.getByCondition(condition);
     } catch (error) {
-<<<<<<< HEAD
       throw new Error(error);
-=======
-      throw new Error("Cant get category by condition");
->>>>>>> ee0b7451de4347f87ae91b1a22834c6e65865503
     }
   }
 }
