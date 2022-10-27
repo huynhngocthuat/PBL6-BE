f652918888aa6b93af85878171df2453d5f6d432
Long-le-mt <denkivip516@gmail.com>
2022-10-27 14:42:10 +0700

    #VSS-12: [feat] Implement api upload image and video

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,10 +2,12 @@ import authController from './auth.controller';
 import CategoryTopicsController from './categoryTopics.controller';
 import CoursesController from './courses.controller';
 import SectionsController from './sections.controller';
+import UploadController from './upload.controller';
 
 export {
   authController,
   CategoryTopicsController,
   CoursesController,
   SectionsController,
+  UploadController,
 };
