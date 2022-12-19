520167f2ef0db1aa02fe92c99e022504eceeca7c
Long-le-mt <denkivip516@gmail.com>
2022-12-20 02:07:35 +0700

    Get view of video

--- src/repositories/videoViews.repository.js
+++ src/repositories/videoViews.repository.js
@@ -1,11 +1,30 @@
 import db from 'models';
 import BaseRepository from 'commons/base.repository';
+import { Sequelize } from 'sequelize';
 
 const { VideoView } = db;
 
 export class VideoViewsRepository extends BaseRepository {
   constructor(model) {
     super(model);
+  }
+
+  async getViewOfVideo(videoId) {
+    try {
+      const data = await this.model.findOne({
+        where: {
+          videoId,
+        },
+        attributes: [
+          [Sequelize.fn('SUM', Sequelize.col('countView')), 'total'],
+        ],
+        group: 'videoId',
+      });
+
+      return data;
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 }
 
