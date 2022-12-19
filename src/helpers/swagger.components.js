e17a2f1c6c15569ee8cf17cad2d43899f50a504e
Lê Huỳnh Nhật Long <72820955+Long-le-mt@users.noreply.github.com>
2022-12-19 17:57:42 +0700

    Update version of swagger (#52)

---
+++ src/helpers/swagger.components.js
@@ -0,0 +1,71 @@
+/**
+ * @swagger
+ * components:
+ *   securitySchemes:
+ *      BearerAuth:
+ *          type: http
+ *          scheme: bearer
+ *   schemas:
+ *     CategoryTopicResponse:
+ *       type: object
+ *       required:
+ *         - name
+ *       properties:
+ *         id:
+ *           type: string
+ *           format: uuid
+ *           description: The auto-generated id of the category
+ *         name:
+ *           type: string
+ *           description: The name of the category topic
+ *
+ *     CategoryTopicRequest:
+ *       type: object
+ *       required:
+ *         - name
+ *       properties:
+ *         name:
+ *           type: string
+ *           description: The name of the category topic
+ *       example:
+ *         name: "FrontEnd"
+ *     Pagination:
+ *       type: object
+ *       properties:
+ *         total_pages:
+ *           type: integer
+ *           description: Total pages of list data
+ *         prev_page:
+ *           type: integer
+ *           description: Previouse page againts current page, is null if current page is first page
+ *         current_page:
+ *           type: integer
+ *           description: Current page
+ *         next_page:
+ *           type: integer
+ *           description: Next page of current page, is null if current page is maximum
+ *         total_count:
+ *           type: integer
+ *           description: Total items of type data queryj
+ *
+ *     ErrorBadRequest:
+ *       type: object
+ *       properties:
+ *         status:
+ *           type: string
+ *           description: Status is failure
+ *         error:
+ *           type: object
+ *           properties:
+ *             message:
+ *               type: string
+ *               description: Description of the error encountered
+ *             code:
+ *               type: integer
+ *               example: 400
+ *               description: HTTP response status codes indicate whether a specific HTTP request has been successfully completed
+ *             errors:
+ *               type: string
+ *               description: Error of request
+ *
+ */
