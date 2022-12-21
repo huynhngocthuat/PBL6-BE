584861b4eb2beaf2eae8039be9dfb7b5260057fd
Long-le-mt <denkivip516@gmail.com>
2022-12-22 01:41:54 +0700

    Validate api anwsswer request become instructor

--- src/routers/admin/users.routes.js
+++ src/routers/admin/users.routes.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import { UserController } from 'controllers';
 import { ValidatorBody, ValidatorId } from 'validations';
+import { userStatussService } from 'services';
+import Response from 'helpers/response';
+import { status, errors } from 'constants';
+import { json } from 'utils';
 
 const router = express.Router();
 
@@ -16,6 +20,28 @@ router.get('/detail/:id', ValidatorId, UserController.getInforDetailOfUser);
 router.post(
   '/requests/answer-request-instructor',
   ValidatorBody('answerRequest'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.body;
+      const data = await userStatussService.findOneByCondition({
+        id,
+      });
+
+      const jsonData = json(data);
+      if (jsonData.status === status.WAITING_STATUS) {
+        next();
+      }
+
+      return Response.error(res, {
+        message: errors.ERR_WHILE_ACTION_NOT_VALID,
+      });
+    } catch (error) {
+      console.log(error);
+      return Response.error(res, {
+        message: errors.ERR_WHILE_ANS_REQUEST_BECOME_INSTRUCTOR,
+      });
+    }
+  },
   UserController.answerRequestBecomeToInstructor
 );
 
