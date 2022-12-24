a313b5a1d0a9314807e30292ac20338d4d4394a5
Long-le-mt <denkivip516@gmail.com>
2022-12-25 05:18:55 +0700

    Detach router for admin

--- src/routers/admin/index.js
+++ src/routers/admin/index.js
@@ -1,10 +1,17 @@
 import express from 'express';
 import users from './users.routes';
 import statistics from './statistics.routes';
+import auth from './auth.routes';
 import soldCourses from './sold-courses.routes';
+import categoryTopics from './categoryTopics.routes';
 
 const router = express.Router();
 
+router.use('', auth);
+
+// middleware check authen and author
+
+router.use('/category-topics', categoryTopics);
 router.use('/users', users);
 router.use('/statistics', statistics);
 router.use('/sold-courses', soldCourses);
