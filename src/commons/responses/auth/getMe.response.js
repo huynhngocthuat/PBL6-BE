887e213dbf7628e028df777dacb783e24874119a
Long-le-mt <denkivip516@gmail.com>
2022-12-24 18:19:27 +0700

    Add field creat at in endpoint get me

--- src/commons/responses/auth/getMe.response.js
+++ src/commons/responses/auth/getMe.response.js
@@ -1,9 +1,10 @@
 class GetMeResponse {
-  constructor({ email, fullName, role, id, avatarUrl }) {
+  constructor({ email, fullName, role, id, avatarUrl, createdAt }) {
     this.email = email;
     this.fullName = fullName;
     this.role = role;
     this.userId = id;
+    this.createdAt = createdAt;
     this.avatarUrl = avatarUrl || null;
   }
 }
